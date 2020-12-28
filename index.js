84a28d31c70aea2529d66e8ede8751c2bf9e7859
sally al sardouk <sally.sardouk98@gmail.com>
2020-12-28 15:03:53 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+class Scooter {
+  constructor(year,color,model){
+    this.year=year
+    this.model=model
+    this.color=color
+  }
+  
+}
+class Driver{
+  constructor(name,age,experience){
+    this.name=name
+    this.age=age
+    this.experience=experience
+  }
+}
+class PickupLocation{
+  constructor(address,city){
+    this.address=address
+    this.city=city
+  }
+}
