3b0d7a63dd290bcfdf08194987053204ca6ca070
sally al sardouk <sally.sardouk98@gmail.com>
2020-12-28 15:03:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,16 @@ class Scooter {
   }
   
 }
+class Driver{
+  constructor(name,age,experience){
+    this.name=name
+    this.age=age
+    this.experience=experience
+  }
+}
+class PickupLocation{
+  constructor(address,city){
+    this.address=address
+    this.city=city
+  }
+}
